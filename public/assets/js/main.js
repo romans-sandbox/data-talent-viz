7819755be06ebeb64c7d0487d53f3cc778bcb6ee
romans <romans.dev@gmail.com>
2016-03-10 17:14:45 +0000

    sample pack

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,10 +4,10 @@ tweetPackChart = function() {
   var width = 500;
   var height = 500;
   var margins = {
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0
+    top: 1,
+    right: 1,
+    bottom: 1,
+    left: 1
   };
 
   var options = {};
@@ -16,6 +16,121 @@ tweetPackChart = function() {
   var availHeight = height - margins.top - margins.bottom;
 
   var svg, mainGroup;
+  var pack, packNodes;
+  var itemsSel, itemsEnter, itemCircles;
+
+  // sample data
+  var data = {
+    "t": "0",
+    "children": [
+      {
+        "t": "00",
+        "value": 2
+      },
+      {
+        "t": "01",
+        "children": [
+          {
+            "t": "010",
+            "value": 1
+          },
+          {
+            "t": "011",
+            "value": 1
+          },
+          {
+            "t": "012",
+            "value": 1
+          },
+          {
+            "t": "013",
+            "value": 1
+          }
+        ]
+      },
+      {
+        "t": "02",
+        "children": [
+          {
+            "t": "020",
+            "value": 2
+          },
+          {
+            "t": "021",
+            "value": 1
+          },
+          {
+            "t": "022",
+            "value": 1
+          },
+          {
+            "t": "023",
+            "value": 1
+          },
+          {
+            "t": "024",
+            "value": 1
+          },
+          {
+            "t": "025",
+            "value": 2
+          }
+        ]
+      },
+      {
+        "t": "03",
+        "children": [
+          {
+            "t": "030",
+            "value": 2
+          },
+          {
+            "t": "031",
+            "value": 2
+          },
+          {
+            "t": "032",
+            "value": 2
+          },
+          {
+            "t": "033",
+            "value": 2
+          }
+        ]
+      },
+      {
+        "t": "04",
+        "value": 5
+      }
+    ]
+  };
+
+  function preparePack() {
+    pack = d3.layout.pack()
+      .size([availWidth, availHeight]);
+
+    packNodes = pack.nodes(data);
+
+    itemsSel = mainGroup.selectAll('circle')
+      .data(packNodes);
+
+    itemsEnter = itemsSel.enter();
+
+    itemCircles = itemsEnter.append('circle')
+      .attr('cx', function(d) {
+        return d.x
+      })
+      .attr('cy', function(d) {
+        return d.y
+      })
+      .attr('r', function(d) {
+        return d.r
+      })
+      .attr('stroke', 'black')
+      .attr('stroke-width', '1')
+      .attr('fill', 'white');
+
+  }
 
   module.run = function() {
     svg = d3.select('#tweet-pack-chart')
@@ -25,6 +140,8 @@ tweetPackChart = function() {
     mainGroup = svg.append('g')
       .attr('class', 'main-group')
       .attr('transform', 'translate(' + margins.left + ', ' + margins.top + ')');
+
+    preparePack();
   };
 
   return module;
